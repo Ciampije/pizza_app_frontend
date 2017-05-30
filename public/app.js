c9a6309a47092d43d3bbf846d2b0f3f338716202
Ciampije <josephciampi@C1MRQ06AHF4H.fios-router.home>
2017-05-30 02:00:56 -0400

    initial reviews

--- public/app.js
+++ public/app.js
@@ -6,6 +6,8 @@ app.controller('mainController', ['$http', function($http){
     this.url = 'http://localhost:3000';
     this.user = {};
     this.message = "angular works!";
+    this.selected_partial = 'index';
+    this.review = ""
     var controller = this;
 
     this.login = function(userPass) {
@@ -16,9 +18,16 @@ app.controller('mainController', ['$http', function($http){
             data: { user: { username: userPass.username, password: userPass.password }},
         }).then(function(result) {
             console.log(result);
+            if (result.data.status == 401) {
+                this.error = "Unauthorized";
+            } else {
+                this.selected_partial = 'userindex';
+
+            }
             this.user = result.data.user;
             localStorage.setItem('token', JSON.stringify(result.data.token));
         }.bind(this));
+
     };
 
     this.getUsers = function() {
@@ -34,6 +43,7 @@ app.controller('mainController', ['$http', function($http){
                 this.error = "Unauthorized";
             } else {
                 this.users = result.data;
+
             }
         }.bind(this))
     };
@@ -53,5 +63,36 @@ app.controller('mainController', ['$http', function($http){
             console.log(controller.restaurants);
         });
     };
+    this.getSpecificRestaurant = function(id) {
+        this.restaurant_id = id;
+        $http({
+            method: 'GET',
+            url: 'http://localhost:3000/users/restaurants/' + id
+        }).then(function(result){
+            this.specificRestaurant = result.data
+            console.log(controller.specificRestaurant);
+            controller.selected_partial = 'restaurantshowpage';
+        }.bind(this));
+    };
+
+    this.addReview = function(review){
+        console.log('hi');
+    $http({
+        method:"POST",
+        url:'http://localhost:3000/users/restaurants/2/reviews',
+        data: review.to_json,
+        headers: {
+            Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token'))
+        }
+    }).then(function(response){//success
+      //this also might be a set to the userid so we probably can get rid of one or the other
+            controller.id = response.data.id;
+            console.log(response);
+        },
+      function(response) {//failure
+        console.log(response);
+      });
+};
+
     this.getRestaurants();
 }]);
